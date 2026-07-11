cca8fe9494272f8abd59db35e7301ee7e2e58479
sonugarg174-blip <sonugarg174@gmail.com>
2026-07-11 11:20:59 +0530

    Add basic Java program to calculate sum

---
+++ acp1.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 20;
+        
+        // Performing addition
+        int sum = number1 + number2;
+        
+        System.out.println("The sum is: " + sum); 
+    }
+}
